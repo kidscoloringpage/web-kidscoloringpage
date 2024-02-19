09fe9671ecbc82e6ce615803a74236e9157cc782
Adnan Ahmed <idnan.se@gmail.com>
2024-02-18 21:49:26 -0500

    update coloring images

---
+++ src/components/ColoringImage.tsx
@@ -0,0 +1,34 @@
+export interface ColoringImageProps {
+    src: string;
+    alt: string;
+    title: string;
+    downloaded: string;
+}
+
+export function ColoringImage(props: ColoringImageProps) {
+    const {src, alt, title, downloaded } = props;
+
+    return (
+        <div
+            className="flex flex-col border border-black rounded-2xl p-4 md:p-6 gap-y-4 relative bg-[#F4F3F3] justify-between">
+            { downloaded &&
+                <p className="bg-[#F36A3B] py-1.5 md:py-2 px-2 md:px-3 w-fit text-white font-sansita font-medium md:font-bold rounded-3xl text-xs md:text-sm top-2 md:top-3 left-2 md:left-3 absolute">Colored
+                    by {downloaded} Kids</p>}
+            <img src={src} alt={alt} className="h-fit bg-cover"/>
+            <div className="flex flex-col gap-y-1.5 items-start">
+            <p className="font-sansita font-bold">{title}</p>
+                <button
+                    onClick={() => {
+                        const a = document.createElement('a');
+                        a.href = src;
+                        a.download = src.split('/').pop();
+                        document.body.appendChild(a);
+                        a.click();
+                        document.body.removeChild(a);
+                    }}
+                    className="text-[#6A7DF6] font-sansita font-bold border-b border-b-[#6A7DF6] leading-snug hover:text-[#F36A3B] hover:border-b-[#F36A3B]">Download
+                </button>
+            </div>
+        </div>
+    );
+}
