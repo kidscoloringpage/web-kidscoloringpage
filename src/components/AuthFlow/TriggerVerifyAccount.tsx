da33f721c4f2150c3ae4faa7c77d88c895040488
Adnan Ahmed <idnan.se@gmail.com>
2024-02-21 22:08:43 -0500

    verify user using verification code

---
+++ src/components/AuthFlow/TriggerVerifyAccount.tsx
@@ -0,0 +1,80 @@
+import {createTokenCookie} from "../../lib/jwt.ts";
+import {redirectAuthSuccess} from "../../lib/auth-redirect.ts";
+import {httpPost} from "../../lib/http.ts";
+import {useEffect, useState} from "react";
+import {LoadingMessage} from "../Loaders/LoadingMessage.tsx";
+
+export function TriggerVerifyAccount() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState('');
+
+    const triggerVerify = (code: string) => {
+        setIsLoading(true);
+
+        httpPost<{ token: string }>(
+            `${import.meta.env.PUBLIC_API_URL}/v1-verify-account`,
+            {
+                code,
+            },
+        )
+            .then(({ response, error }) => {
+                if (!response?.token) {
+                    setError(error?.message || 'Something went wrong. Please try again.');
+                    setIsLoading(false);
+
+                    return;
+                }
+
+                createTokenCookie(response.token);
+                redirectAuthSuccess();
+            })
+            .catch(() => {
+                setIsLoading(false);
+                setError('Something went wrong. Please try again.');
+            });
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code')!;
+
+        if (!code) {
+            setIsLoading(false);
+            setError('Something went wrong. Please try again later.');
+            return;
+        }
+
+        triggerVerify(code);
+    }, []);
+
+    const loadingMessage = isLoading && (
+        <LoadingMessage message="Please wait while we verify you..." />
+    );
+
+    const errorMessage = error && (
+        <div className="flex flex-col justify-center items-center">
+            <div className="w-20 h-20 md:w-40 md:h-40 text-red-700">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                     stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"
+                     className="lucide lucide-alert-triangle">
+                    <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z"/>
+                    <path d="M12 9v4"/>
+                    <path d="M12 17h.01"/>
+                </svg>
+            </div>
+            <h2 className="mb-2 text-xl md:text-4xl font-semibold font-sansita text-red-700">Oops!</h2>
+            <div className="text-base md:text-2xl">
+                <p className="text-red-700">{error}</p>
+            </div>
+        </div>
+    );
+
+    return (
+        <div className="flex flex-col items-center w-full py-14">
+            <div className="text-center">
+                {loadingMessage}
+                {errorMessage}
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Loaders/LoadingMessage.tsx
@@ -0,0 +1,23 @@
+import { DefaultSpinner } from './DefaultSpinner';
+
+type LoadingMessageProps = {
+  message: string;
+};
+
+export function LoadingMessage(props: LoadingMessageProps) {
+  const { message } = props;
+
+  return (
+    <div>
+      <div className="bg-black/80 fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center backdrop-blur">
+        <div className="flex items-center justify-center rounded-md border bg-white px-4 py-2 shadow-lg">
+          <DefaultSpinner className={'h-4 w-4 md:h-5 md:w-5'} innerFill="#000000" />
+          <h1 className="ml-2 text-base md:text-xl font-light">
+            {message}
+            <span className="animate-pulse"></span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
