f670783142630d42e47270877759926507bf92da
Adnan Ahmed <idnan.se@gmail.com>
2024-03-01 13:49:46 -0500

    add lazy loading on images

--- src/components/ColoringImage.tsx
+++ src/components/ColoringImage.tsx
@@ -30,25 +30,28 @@ export function ColoringImage(props: ColoringImageProps) {
   return (
     <div className="relative flex flex-col justify-between gap-y-4 rounded-2xl border border-black bg-white px-4 pb-4 group-hover:border-[#F36A3C] group-hover:text-[#F36A3C] md:px-6 md:pb-6">
       {downloaded && loaded && (
-        <p className="absolute left-2 top-2 w-fit rounded-3xl bg-[#F36A3B] px-2 py-1.5 font-sansita text-xs font-medium text-white md:left-3 md:top-3 md:px-3 md:py-2 md:text-sm md:font-bold">
+        <p className="absolute left-2 top-2 z-40 w-fit rounded-3xl bg-[#F36A3B] px-2 py-1.5 font-sansita text-xs font-medium text-white md:left-3 md:top-3 md:px-3 md:py-2 md:text-sm md:font-bold">
           Colored by {downloaded} Kids
         </p>
       )}
-      {src && (
-        <img
-          ref={ref}
-          onLoad={onLoad}
-          src={src}
-          alt={alt}
-          className={`h-auto w-full object-cover ${loaded ? 'block' : 'hidden'}`}
-        />
-      )}
-      {!src ||
-        (!loaded && (
-          <div className="h-full min-h-[189px] pt-5 md:min-h-[349px]">
-            <Skeleton count={1} height="100%" duration={0.9} />
-          </div>
-        ))}
+      <div className="relative flex flex-col">
+        {src && (
+          <img
+            ref={ref}
+            onLoad={onLoad}
+            src={src}
+            alt={alt}
+            loading="lazy"
+            className={`h-auto w-full object-cover ${loaded ? 'opacity-100' : 'opacity-0'}`}
+          />
+        )}
+        {!src ||
+          (!loaded && (
+            <div className="absolute left-0 top-0 h-full w-full pt-4">
+              <Skeleton count={1} height="100%" duration={0.9} />
+            </div>
+          ))}
+      </div>
       <div className="flex flex-col items-start gap-y-1.5">
         <p className="w-full truncate font-sansita font-bold capitalize">
           {title}
