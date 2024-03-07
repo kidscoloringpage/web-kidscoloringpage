28d4e25f8c9e29b3cd181a1a8b8ca747e1e16626
Adnan Ahmed <idnan.se@gmail.com>
2024-03-06 23:54:12 -0500

    christmas coloring page

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -107,6 +107,9 @@ export const Footer = (props: { paymentSessionId: string }) => {
                 <a href="/dinosaur-coloring-pages" className="hover:underline">
                   Dinosaur Coloring Pages
                 </a>
+                <a href="/christmas-coloring-pages" className="hover:underline">
+                  Christmas Coloring Pages
+                </a>
                 <a href="/marvel-coloring-pages" className="hover:underline">
                   Marvel Coloring Pages
                 </a>
