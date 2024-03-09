730ce278b3c5eecfbd08b9f41c77d48f86282bc7
Adnan Ahmed <idnan.se@gmail.com>
2024-03-09 14:45:26 -0500

    change the behaviour of download image

--- src/components/ColoringImage.tsx
+++ src/components/ColoringImage.tsx
@@ -26,7 +26,23 @@ export function ColoringImage(props: ColoringImageProps) {
   });
 
   return (
-    <div className="relative flex flex-col justify-between gap-y-4 rounded-2xl border border-black bg-white px-4 pb-4 group-hover:border-[#F36A3C] group-hover:text-[#F36A3C] md:px-6 md:pb-6">
+    <div
+      onClick={() => {
+        if (!allowDownload || !src) {
+          return;
+        }
+        const a = document.createElement('a');
+        a.href = src;
+        a.download = src.split('/').pop();
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }}
+      data-ga-category="DownloadClick"
+      data-ga-action="ColoringPageDownload"
+      data-ga-label={title}
+      className="group relative flex cursor-pointer flex-col justify-between gap-y-4 rounded-2xl border border-black bg-white px-4 pb-4 transition hover:drop-shadow-lg group-hover:border-[#F36A3C] group-hover:text-[#F36A3C] md:px-6 md:pb-6"
+    >
       {downloaded && loaded && (
         <p className="absolute left-2 top-2 z-40 w-fit rounded-3xl bg-[#F36A3B] px-2 py-1.5 font-sansita text-xs font-medium text-white md:left-3 md:top-3 md:px-3 md:py-2 md:text-sm md:font-bold">
           Colored by {downloaded} Kids
@@ -56,22 +72,9 @@ export function ColoringImage(props: ColoringImageProps) {
         </p>
         {src && allowDownload && (
           <div className="flex w-full items-center justify-between">
-            <button
-              onClick={() => {
-                const a = document.createElement('a');
-                a.href = src;
-                a.download = src.split('/').pop();
-                document.body.appendChild(a);
-                a.click();
-                document.body.removeChild(a);
-              }}
-              data-ga-category="DownloadClick"
-              data-ga-action="ColoringPageDownload"
-              data-ga-label={title}
-              className="border-b border-b-[#6A7DF6] font-sansita font-bold leading-snug text-[#6A7DF6] hover:border-b-[#F36A3B] hover:text-[#F36A3B]"
-            >
+            <p className="font-sansita font-bold leading-snug text-[#6A7DF6] group-hover:text-[#F36A3B]">
               Download
-            </button>
+            </p>
           </div>
         )}
         {!src && <p className="">Generating Image please wait...</p>}
