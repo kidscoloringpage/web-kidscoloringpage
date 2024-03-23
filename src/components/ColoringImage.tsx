3d01842d61d46db265f8bc54e31a4ac5d503f20b
Adnan Ahmed <idnan.se@gmail.com>
2024-03-22 22:56:16 -0400

    add button to print coloring image

--- src/components/ColoringImage.tsx
+++ src/components/ColoringImage.tsx
@@ -8,15 +8,45 @@ export interface ColoringImageProps {
   title: string;
   downloaded?: string;
   allowDownload?: boolean;
+  allowPrint?: boolean;
 }
 
 export function ColoringImage(props: ColoringImageProps) {
-  const { src, alt, title, downloaded, allowDownload = true } = props;
+  const {
+    src,
+    alt,
+    title,
+    downloaded,
+    allowDownload = true,
+    allowPrint = true,
+  } = props;
   const [loaded, setLoaded] = useState(false);
   const ref = useRef<HTMLImageElement>(null);
 
   const onLoad = () => {
     setLoaded(true);
+  };
+
+  const print = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e?.stopPropagation();
+
+    // Define the dimensions of the popup.
+    const popupWidth = 600;
+    const popupHeight = 600;
+
+    // Calculate the position to center the popup.
+    const left = window.screen.width / 2 - popupWidth / 2;
+    const top = window.screen.height / 2 - popupHeight / 2;
+
+    const features = `width=${popupWidth},height=${popupHeight},left=${left},top=${top}`;
+
+    const printWindow = window.open('', '', features);
+    printWindow.document.write(
+      `<img src="${src}" alt="coloring-image" style="width:100%;height:100%;object-fit:contain;" />`,
+    );
+    printWindow.document.close();
+    printWindow.focus();
+    printWindow.print();
   };
 
   useEffect(() => {
@@ -43,6 +73,30 @@ export function ColoringImage(props: ColoringImageProps) {
       data-ga-label={title}
       className="group relative flex cursor-pointer flex-col justify-between gap-y-4 rounded-2xl border border-black bg-white px-4 pb-4 transition hover:drop-shadow-lg group-hover:border-[#F36A3C] group-hover:text-[#F36A3C] md:px-6 md:pb-6"
     >
+      {allowPrint && loaded && (
+        <button
+          onClick={print}
+          className="absolute right-2 top-2 z-40 hidden rounded-lg bg-[#F36A3B] group-hover:block md:right-3 md:top-3"
+          title="print"
+        >
+          <div className="group/icon-container p-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              stroke="#FFFFFF"
+              fill="#F36A3B"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="h-4 w-4 text-white md:h-5 md:w-5"
+            >
+              <polyline points="6 9 6 2 18 2 18 9" />
+              <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
+              <rect width="12" height="8" x="6" y="14" />
+            </svg>
+          </div>
+        </button>
+      )}
       {downloaded && loaded && (
         <p className="absolute left-2 top-2 z-40 w-fit rounded-3xl bg-[#F36A3B] px-2 py-1.5 font-sansita text-xs font-medium text-white md:left-3 md:top-3 md:px-3 md:py-2 md:text-sm md:font-bold">
           Colored by {downloaded} Kids
